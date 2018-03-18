5c7c8e2d9a242cd61660318e44719c06b2d67368
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-03-18 22:09:27 +0800

    Added walls
    Created player prefab
    Made player movement slower and more consistent

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	private List<Player> clones = new List<Player>();
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		Vector2 pos = transform.position;
+		float dy = Input.GetAxis ("Vertical");
+		if (dy > 0) {
+			pos.y += 0.1f;
+		} else if (dy < 0) {
+			pos.y -= 0.1f;
+		}
+		transform.position = pos;
 	}
 }
