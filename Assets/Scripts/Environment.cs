6754b882b4fb5398a434f94bc628c99484ff68b5
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-03-19 17:27:30 +0800

    Added Environment script
    Can create copies of player by clicking

---
+++ Assets/Scripts/Environment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment : MonoBehaviour {
+
+	[SerializeField] private Camera cam;
+
+	[SerializeField] private List<Player> players;
+	[SerializeField] private Player playerPrefab;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 pos = Input.mousePosition;
+			pos.z = 0;
+			pos = cam.ScreenToWorldPoint (pos);
+			Instantiate (
+				playerPrefab,
+				pos,
+				Quaternion.identity
+			);
+		}
+	}
+
+}
