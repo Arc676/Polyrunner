bb7b14135a3658188a886800d00bd2b4e1ab5413
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-03-19 17:11:45 +0800

    Movement now based on W/S key down rather than axis
    Prevent continued movement after releasing keys

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,23 @@ public class Player : MonoBehaviour {
 	
 	private bool isControlled = true;
 
+	private const int playerLayerMask = ~(1 << 8);
+
 	void Update () {
 		if (isControlled) {
 			Vector2 pos = transform.position;
-			float dy = Input.GetAxis ("Vertical");
+			float dy = 0;
+			int sgn = 0;
+			if (Input.GetKey (KeyCode.W)) {
+				dy = 0.1f;
+				sgn = 1;
+			} else if (Input.GetKey (KeyCode.S)) {
+				dy = -0.1f;
+				sgn = -1;
+			}
 			if (dy != 0) {
-				int sgn = (int)Mathf.Sign (dy);
 				dy = sgn * 0.1f;
-				RaycastHit2D hit = Physics2D.Raycast (pos, sgn * Vector2.up, 0.2f, ~(1 << 8));
+				RaycastHit2D hit = Physics2D.Raycast (pos, sgn * Vector2.up, 0.2f, playerLayerMask);
 				if (hit.collider == null) {
 					pos.y += dy;
 					transform.position = pos;
