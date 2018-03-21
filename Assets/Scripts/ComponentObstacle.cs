1c02c587db449bd983aa1d13ccee38931a2ca66c
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-03-21 22:09:44 +0800

    Created scripts for compound obstacles

---
+++ Assets/Scripts/ComponentObstacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentObstacle : MonoBehaviour {
+
+	private bool passed = false;
+
+	public void pass() {
+		passed = true;
+	}
+
+	public bool hasBeenPassed() {
+		return passed;
+	}
+
+}
---
+++ Assets/Scripts/CompoundObstacle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompoundObstacle : MonoBehaviour {
+
+	private List<ComponentObstacle> components = new List<ComponentObstacle>();
+
+	public void addComponent(ComponentObstacle c) {
+		components.Add (c);
+	}
+
+	public bool componentsPassed() {
+		foreach (ComponentObstacle c in components) {
+			if (!c.hasBeenPassed ()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
